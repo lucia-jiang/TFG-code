d56caba366473ec13b1890748cc7fb5e55c12db7
Lucia-jiang <lu.jiang@alumnos.upm.es>
2023-02-25 23:45:52 +0100

    Clasificación por traza y determinante

--- tfg.py
+++ tfg.py
@@ -222,8 +222,39 @@ def clasificar_punto_autoval(a,b,c,d):
 
 
 '''------------------CLASIFICAR PUNTO POR TRAZA Y DETERMINANTE----------------'''
+def clasificar_traza(a,b,c,d):
+    T = a+d #traza
+    D = a*d-b*c #determinante
+    if T**2-4*D < 0:
+        print('Autovalores complejos')
+        if T < 0:
+            print('El punto (0,0) es un foco estable')
+        elif T > 0:
+            print('El punto (0,0) es un foco inestable')
+        else:  # T=0
+            print('El punto (0,0) es un centro estable')
+    elif T**2-4*D > 0:
+        print('Autovalores reales y distintos')
+        if D<0:
+            print('El punto (0,0) es un punto de silla')
+        elif T>0 and D>0:
+            print('El punto (0,0) es un punto inestable')
+        elif T<0 and D>0:
+            print('El punto (0,0) es un punto estable')
+        elif D==0 and T!=0:
+            print('Un autovalor 0')
+        elif D==0 and T==0:
+            print('Los dos autovalores 0')
+    else: # T**2-4*D = 0
+        print('Autovalores reales y repetidos')
 
-
+        if b == 0 and c == 0: #TODO: revisar
+            print('El punto (0,0) es un punto estelar') #autovalor > 0 -> se alejan del origen y vicerversa
+        else:
+            if T > 0:
+                print('El punto (0,0) es un nodo impropio inestable')
+            else: #autoval < 0
+                print('El punto (0,0) es un nodo impropio estable')
 
 '''------------------ECUACIÓN DE SEGUNDO ORDEN----------------'''
 #TODO: pasar ecuaciones lineales de segundo orden a un sistema (incluso cualquier orden y solo poner como sistema)
@@ -262,7 +293,7 @@ def segundo_orden(a,b,c):
 # clasificar_punto_autoval(1,0,0,2) #punto inestable
 # clasificar_punto_autoval(0,1,-1,0) #autovalores complejos puro y centro estable
 # clasificar_punto_autoval(2,1,-1,2) #autovalores complejos mixto y foco inestable
-# clasificar_punto_autoval(-2,1,-1,-2) #autovalores complejos mixo y foco estable
+# clasificar_punto_autoval(-2,1,-1,-2) #autovalores complejos mixto y foco estable
 # clasificar_punto_autoval(1,0,0,1) #autovalor real repetido
 # clasificar_punto_autoval(2,0,3,2) #autovalor real repetido
 # clasificar_punto_autoval(3,2,0,3) #autovalor real repetido
